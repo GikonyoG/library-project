ad54897a3e51261c8217761b1eb746712fc541ea
victormusembi11 <victor.musembi11@gmail.com>
2022-12-15 01:52:20 +0300

    feat: is book available

--- LibraryProject copy 2/bookCheckout.py
+++ LibraryProject copy 2/bookCheckout.py
@@ -2,16 +2,22 @@ from database import *
 
 
 def is_book_available(book_id, member_id):
+    latest = None
+
     if int(member_id) < 1000 or int(member_id) > 9999:
         print("Member ID is invalid")
     else:
       for loan in read_log_records():
-        if loan[0] == book_id and loan[-1] == '':
-            print("Sorry, this book is not available. It has been loaned out")
+        if loan[0] == book_id:
+            latest =  loan
+
+    if latest[-1] == "''":
+        print("Sorry, this book is not available. It has been loaned out")
+
+    return latest
 
 
-
-is_book_available('5', '1111')
+print(is_book_available('5', '1111'))
 
 
 def member_check(member_id):
